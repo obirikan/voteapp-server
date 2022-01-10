28a2d55aa8424c0557e00d6a259e756c721aa201
unknown <obirikan020@gmail.com>
2022-01-10 03:50:29 -0800

    added poll routes

--- handlers/poll.js
+++ handlers/poll.js
@@ -3,6 +3,8 @@ const db=require('../models')
 exports.showpoll=async (req,res,next)=>{
   try{
    const polls=await db.polls.find()
+   
+   
 
    res.status(200).json(polls)
   }catch(err){
@@ -13,14 +15,22 @@ exports.showpoll=async (req,res,next)=>{
 
 exports.createPoll=async (req,res,next)=>{
     try{
+    const {id}=req.decoded
+
+    const user= await db.user.findById(id)
+
     const {question,options}=req.body
+
     const poll=await db.polls.create({
+        user,
         question,
         options:options.map(option=>({
             option,
             votes:0
         }))
     })
+    user.poll.push(poll._id);
+    await user.save()
 
       res.status(201).json(poll)
     }catch(err){
